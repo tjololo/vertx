06a840163cddbfa2a4f37c7a6131f423e8fb8210
tjololo <vemundg@gmail.com>
2016-10-22 22:19:41 +0200

    initial commit

---
+++ http-first/src/test/java/net/tjololo/poc/vertx/AppTest.java
@@ -0,0 +1,14 @@
+package net.tjololo.poc.vertx;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class AppTest {
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
